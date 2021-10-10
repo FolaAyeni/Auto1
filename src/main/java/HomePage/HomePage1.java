300faa1a00a32e53b82d393d342e88309a5baf09
Auto1 <fola.ayeni@yahoo.com>
2021-10-10 20:42:08 +0100

    HomePage_01 beginning

---
+++ src/main/java/HomePage/HomePage1.java
@@ -0,0 +1,15 @@
+package HomePage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.webUtils;
+
+public class HomePage1 extends webUtils {
+    public HomePage1(WebDriver driver) {
+        super(driver);
+    }
+    private final WebElement home = driver.findElement(By.id(""));
+    private final By yyyy = By.id("");
+
+}
